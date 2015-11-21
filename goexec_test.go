819de06fce43b9ec8a386a05d5bcf3e45ff53002
Dmitri Shuralyov <shurcooL@gmail.com>
2015-11-20 23:50:32 -0800

    Add test for run.

---
+++ goexec_test.go
@@ -0,0 +1,18 @@
+package main
+
+func ExampleRun() {
+	const src = `package main
+
+import "github.com/shurcooL/go-goon"
+
+func main() {
+	goon.Dump("goexec's run() is working.")
+}
+`
+	err := run(src)
+	if err != nil {
+		panic(err)
+	}
+
+	// Output: (string)("goexec's run() is working.")
+}
